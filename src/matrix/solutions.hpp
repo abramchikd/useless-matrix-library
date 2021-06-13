97eafa15551ddfd3270a488a8a289c18543168ce
Dmitrij Abramov <dimabradima@gmail.com>
2021-06-14 00:48:58 +0300

    Solutions.hpp fixed

--- src/matrix/solutions.hpp
+++ src/matrix/solutions.hpp
@@ -1,6 +1,8 @@
 #ifndef USELESS_MATRIX_LIBRARY_SOLUTIONS_HPP
 #define USELESS_MATRIX_LIBRARY_SOLUTIONS_HPP
 
+#include <vector>
+
 namespace mat {
 
 enum class solutions_type {
@@ -10,9 +12,9 @@ enum class solutions_type {
 struct solutions {
     solutions_type type;
     size_t number_of_solutions = -1;
-    std::vector solutions = {};
+    std::vector<double> sol = {};
 
-    solutions(solutions_type type, size_t number_of_solutions = -1, std::vector const& solutions = {});
+    solutions(solutions_type type, size_t number_of_solutions = -1, std::vector<double> const& sol = {});
 };
 } // namespace mat
 
