135175917a7614ebe26e38a447dd7a4a34c35a66
Dmitrij Abramov <dimabradima@gmail.com>
2021-06-13 05:42:44 +0300

    added tests of matrix constructors and operator[]

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,14 @@
 #include "catch.hpp"
 
-#include "matrix/matrix_row.hpp"
+#include <iostream>
 
-TEST_CASE("mat::matrix_row(int) constructor test") {
+#include "matrix/matrix_row.hpp"
+#include "matrix/matrix.hpp"
+
+TEST_CASE("mat::matrix_row(size_t) constructor test") {
     auto row = mat::matrix_row<int>(3);
     CHECK(row.get_width() == 3);
-    for (int i = 0; i < 3; i++) {
+    for (size_t i = 0; i < 3; i++) {
         CHECK(row[i] == 0);
     }
 }
@@ -13,13 +16,38 @@ TEST_CASE("mat::matrix_row(int) constructor test") {
 TEST_CASE("mat::matrix_row(std::initializer_list<T>) constructor test") {
     auto row = mat::matrix_row {0, 1, 2, 3};
     CHECK(row.get_width() == 4);
-    for (int i = 0; i < 4; i++) {
+    for (size_t i = 0; i < 4; i++) {
         CHECK(row[i] == i);
     }
 }
 
-TEST_CASE("mat::matrix std::out_of_range exception") {
+TEST_CASE("mat::matrix_row throws std::out_of_range exception") {
     auto row = mat::matrix_row<int>(2);
     CHECK_THROWS(row[2]);
     CHECK_THROWS(row[3]);
+}
+
+TEST_CASE("mat::matrix(size_t) constructor") {
+    auto matrix = mat::matrix<int>(2, 2);
+
+    for (size_t i = 0; i < 2; i++) {
+        for (size_t j = 0; j < 2; j++) {
+            CHECK(matrix[i][j] == 0);
+        }
+    }
+}
+
+TEST_CASE("mat::matrix(std::initializer_list<...) constructor") {
+    auto matrix = mat::matrix<int> {{0, 1}, {1, 2}};
+
+    for (size_t i = 0; i < 2; i++) {
+        for (size_t j = 0; j < 2; j++) {
+            CHECK(matrix[i][j] == i + j);
+        }
+    }
+}
+
+TEST_CASE("mat::matrix throws std::out_of_range exception") {
+    auto matrix = mat::matrix<int>(2, 2);
+    CHECK_THROWS(matrix[2]);
 }
