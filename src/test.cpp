2740d9bafdc5794f8b0b5832264c9c57e202d687
Dmitrij Abramov <dimabradima@gmail.com>
2021-06-13 04:29:30 +0300

    Addded tests for matrix_row

--- src/test.cpp
+++ src/test.cpp
@@ -11,9 +11,15 @@ TEST_CASE("mat::matrix_row(int) constructor test") {
 }
 
 TEST_CASE("mat::matrix_row(std::initializer_list<T>) constructor test") {
-    auto row = mat::matrix_row<int> {0, 1, 2, 3};
+    auto row = mat::matrix_row {0, 1, 2, 3};
     CHECK(row.get_width() == 4);
     for (int i = 0; i < 4; i++) {
         CHECK(row[i] == i);
     }
+}
+
+TEST_CASE("mat::matrix std::out_of_range exception") {
+    auto row = mat::matrix_row<int>(2);
+    CHECK_THROWS(row[2]);
+    CHECK_THROWS(row[3]);
 }
